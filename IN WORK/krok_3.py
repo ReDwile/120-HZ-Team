579b6748e320dc0eed2d1e1b4f70a8714de39ab6
adskiireanimator <57593257+adskiireanimator@users.noreply.github.com>
2019-11-29 18:44:04 +0300

    Add files via upload

---
+++ IN WORK/krok_3.py
@@ -0,0 +1,20 @@
+from openpyxl import load_workbook,Workbook
+wbSearch=Workbook()
+wbSearch=load_workbook("search.xlsx")#сюда напиши название своего екселя
+wsSearch=wbSearch.active
+
+lookfor_1="white_list"
+lookfor_2="admin"
+
+def kostya(id):
+    for i in range(1,21): #вместо 21 можно взять любое число
+        value=wsSearch.cell(row=i,column=1).value
+        if value== id:
+            id_status=wsSearch.cell(row=value.row,column=2)
+            if id_status==lookfor_2:
+                return("admin")
+            elif id_status==lookfor_1:
+                return ("pupile")
+        else:
+            return (None)
+
