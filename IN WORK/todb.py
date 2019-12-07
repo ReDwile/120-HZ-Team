b625c93dc085b731d430e598fef7c24d2ea8a71f
adskiireanimator <57593257+adskiireanimator@users.noreply.github.com>
2019-12-07 17:48:12 +0300

    Update todb.py

--- IN WORK/todb.py
+++ IN WORK/todb.py
@@ -3,10 +3,10 @@ wbSearch=Workbook()
 wbSearch=load_workbook('data/.xlsx')#сюда напиши название своего екселя
 wsSearch=wbSearch.active
 
-def todb(id,name,lastname):
+def todb(ID,name,lastname):
     maxrow = wsSearch.max_row
     row =maxrow+1
-    wsSearch.cell(row=row, column=1).value=id
+    wsSearch.cell(row=row, column=1).value =  ID
     wsSearch.cell(row=row, column=3).value = name
     wsSearch.cell(row=row, column=4).value = lastname
 
