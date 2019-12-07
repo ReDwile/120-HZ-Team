c1bca28032e59c700488cfc2fb659edcbf041457
adskiireanimator <57593257+adskiireanimator@users.noreply.github.com>
2019-12-07 13:47:58 +0300

    Add files via upload

--- IN WORK/GetUserActs.py
+++ IN WORK/GetUserActs.py
@@ -7,7 +7,15 @@ def GetUserActs(id):
     kolichestvo = wsSearch.max_row
     for i in range(1,kolichestvo):
         value=wsSearch.cell(row=i,column=1).value
+        zero=[]
+        activities=[]
         if value== id:
-            activities=[wsSearch.cell(row=value.row,column=5).value]
-    else:
-        return None
+            activities.append(wsSearch.cell(row=value.row,column=5).value)
+
+
+        if zero ==activities:
+            return None
+        else:
+            return activities
+
+
