f33a7219dfd81aec00d773e6622f2abbdd1a336c
lalkalol1907 <57593173+lalkalol1907@users.noreply.github.com>
2020-01-02 01:12:26 +0300

    Delete new_excel.py

--- IN WORK/bot/new_excel.py
+++
@@ -1,8 +0,0 @@
-from openpyxl import load_workbook
-import datetime
-
-def code():
-    wb = load_workbook('./test.xlsx')
-    wsSearch = wb.active
-    value = wsSearch.cell(row=wsSearch.max_row, column=1).value
-    return (value)
